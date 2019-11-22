e2b27565b28b4bc5010397c8d7e77438590a089c
Kleber Moreti de Camargo <kleber.camargo@fatec.sp.gov.br>
2019-11-22 22:57:10 +0000

    Added Delete Beer endpoint

--- src/main/java/com/moreti/kmoretibrewery/service/BeerServiceImpl.java
+++ src/main/java/com/moreti/kmoretibrewery/service/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.moreti.kmoretibrewery.service;
 
 import com.moreti.kmoretibrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
     @Override
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
         //todo impl - would add a real impl to update beer
+    }
+
+    @Override
+    public void deleteBeer(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
--- src/main/java/com/moreti/kmoretibrewery/web/controllers/BeerController.java
+++ src/main/java/com/moreti/kmoretibrewery/web/controllers/BeerController.java
@@ -37,5 +37,11 @@ public class BeerController {
         beerService.updateBeer(beerId, beerDto);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
+
+    @DeleteMapping("/{beerId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleDelete(@PathVariable UUID beerId) {
+        beerService.deleteBeer(beerId);
+    }
 }
 
